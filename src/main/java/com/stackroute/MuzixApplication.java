550afaffe27bc33d92e89394e370101b892d9af3
shalini.ganesh <shalini.ganesh@boeing.com>
2019-01-22 22:34:50 +0530

    initial commit

--- src/main/java/com/stackroute/MuzixApplication.java
+++ src/main/java/com/stackroute/MuzixApplication.java
@@ -1,13 +1,30 @@
 package com.stackroute;
 
+import com.stackroute.domain.User;
+import com.stackroute.repository.MuzixRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
 
 @SpringBootApplication
-public class MuzixApplication {
+public class MuzixApplication implements ApplicationListener<ContextRefreshedEvent>, CommandLineRunner {
 
+	@Autowired
+	MuzixRepository muzixRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(MuzixApplication.class, args);
 	}
-}
 
+	@Override
+	public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+		muzixRepository.save(new User(1,"Happy Days","with family"));
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		muzixRepository.save(new User(2,"Work","Work is Worship"));
+	}
+}
